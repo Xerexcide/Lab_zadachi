becf96c71df9c64e944862a517380ae8f8f4ec05
solovyeva-maria <maria.solovyeva@yahoo.com>
2025-02-24 15:46:01 +0300

    m

---
+++ Fourth.py
@@ -0,0 +1,5 @@
+a = int(input("Введите первое число"))
+b = int(input("Введите второе число"))
+c = int(input("Введите третье число"))
+sr_arf = (a + b + c) /  3
+print(sr_arf)
---
+++ second.py
@@ -0,0 +1,4 @@
+a = int(input("Введите а"))
+b = int(input("Введите b"))
+answer = -b/a
+print(answer)
